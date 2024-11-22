3558ca859ff79c108e3f439d3b4c0aa948d51c81
ianchow <13424055869@163.com>
2024-11-22 18:15:13 +0800

    Going to add the async usb control

--- host/usbcontrol.cpp
+++ host/usbcontrol.cpp
@@ -67,6 +67,10 @@ void USBControl::closeUSB()
         libusb_exit(context);
         context = nullptr;
     }
+
+    if (config_descriptor){
+        libusb_free_config_descriptor(config_descriptor);
+    }
 }
 
 bool USBControl::findAndOpenUVCDevice()
@@ -88,8 +92,12 @@ bool USBControl::findAndOpenUVCDevice()
     }
     qCDebug(log_usb) << "Successfully opened and configured device";
 
-    getConfigDescriptor();
-    showConfigDescriptor();
+    int r = libusb_set_auto_detach_kernel_driver(deviceHandle, 1);
+    if (r != 0) {
+        qCDebug(log_usb) << "Failed to detach kernel driver:" << libusb_error_name(r) << libusb_strerror(r);
+    } else {
+        qCDebug(log_usb) << "Successfully detached the device";
+    }
 
     int result = libusb_claim_interface(deviceHandle, 0x00);
     if (result != 0) {
@@ -97,9 +105,14 @@ bool USBControl::findAndOpenUVCDevice()
         return false;
     }
 
+    getConfigDescriptor();
+    showConfigDescriptor();
+    // int brightness = getBrightness();
+    bool init_result = initTransfer();
+    if(result)
+    // getContrastAsync();
+    qCDebug(log_usb) << " contrast get ***************************** ";
     
-    int brightness = getBrightness();
-    qCDebug(log_usb) << "Brightness: " << brightness;
     emit deviceConnected();
     return true;
 }
@@ -107,7 +120,7 @@ bool USBControl::findAndOpenUVCDevice()
 void USBControl::getConfigDescriptor()
 {
     libusb_get_config_descriptor(device, 0 ,&config_descriptor);
-    libusb_free_config_descriptor(config_descriptor);
+    // Allinterface = config_descriptor->interface;
 }
 
 void USBControl::showConfigDescriptor()
@@ -122,26 +135,109 @@ void USBControl::showConfigDescriptor()
     qCDebug(log_usb) << "bmAttributes: " << config_descriptor->bmAttributes;
     qCDebug(log_usb) << "bMaxPower: " << config_descriptor->MaxPower;
     
+    for (int i = 0; i < config_descriptor->bNumInterfaces; i++) {
+        const struct libusb_interface *interface = &config_descriptor->interface[i];
+        for (int j = 0; j < interface->num_altsetting; j++) {
+            const struct libusb_interface_descriptor *altsetting = &interface->altsetting[j];
+            qCDebug(log_usb) << "****************************************************" ;
+            qCDebug(log_usb) << "Interface Number:  " << altsetting->bInterfaceNumber;
+            qCDebug(log_usb) << "  bLength:" << altsetting->bLength;
+            // qCDebug(log_usb) << "Interface Number:  " << altsetting->bInterfaceNumber;
+            qCDebug(log_usb) << "  Alternate Setting:" << altsetting->bAlternateSetting;
+            qCDebug(log_usb) << "  Interface Class:" << altsetting->bInterfaceClass;
+            qCDebug(log_usb) << "  Interface Subclass:" << altsetting->bInterfaceSubClass;
+            qCDebug(log_usb) << "  Interface Protocol:" << altsetting->bInterfaceProtocol;
+            qCDebug(log_usb) << "  Number of Endpoints:" << altsetting->bNumEndpoints;
+            
+            for (int k = 0; k < altsetting->bNumEndpoints; k++) {
+                const struct libusb_endpoint_descriptor *endpoint = &altsetting->endpoint[k];
+                qCDebug(log_usb) << "    Endpoint Address: 0x" << QString::number(endpoint->bEndpointAddress, 16).rightJustified(2, '0');
+                qCDebug(log_usb) << "    Endpoint Attributes: 0x" << QString::number(endpoint->bmAttributes, 16).rightJustified(2, '0');
+                qCDebug(log_usb) << "    Max Packet Size:" << endpoint->wMaxPacketSize;
+                qCDebug(log_usb) << "    Interval:" << endpoint->bInterval;
+            }
+            qCDebug(log_usb) << "\n" ;
+        }
+    }
     
 }
 
 int USBControl::getBrightness()
 {
-    unsigned char data[2];
+    // uint8_t data[2];
     int result;
+    uint8_t 	bmRequestType = 0xA1;
+    uint8_t 	bRequest = GET_CUR;
+    uint16_t 	wValue = PU_BRIGHTNESS_CONTROL;
+    uint16_t 	wIndex = 0x0002;
+    uint8_t 	data[2];
+    uint16_t 	wLength = 2;
+    unsigned int 	timeout = 1000;
     result = libusb_control_transfer(
         deviceHandle,
-        LIBUSB_ENDPOINT_IN | LIBUSB_REQUEST_TYPE_CLASS | LIBUSB_RECIPIENT_INTERFACE,
-        UVC_GET_CUR,
-        (PU_BRIGHTNESS_CONTROL << 8) | INTERFACE_ID,
-        (bDescriptorSubtype << 8) | INTERFACE_ID,
+        bmRequestType,
+        bRequest,
+        wValue,
+        wIndex,
         data,
-        sizeof(data),
-        0
+        wLength,
+        timeout
     );
-    if (result != sizeof(data)) {
-        qCDebug(log_usb) << "Failed to get brightness: " << libusb_strerror(result);
+    
+    // Add error handling for the control transfer
+    if (result < 0) {
+        qCDebug(log_usb) << "Failed to get brightness: " << libusb_strerror(result) << result;
+        // Additional logging for troubleshooting
+        qCDebug(log_usb) << "Control transfer parameters: "
+                         << "Request Type:" << bmRequestType
+                         << "Request:" << bRequest
+                         << "wValue:" << wValue
+                         << "wIndex:" << wIndex;
         return -1;
     }
+    
+    if (result != sizeof(data)) {
+        qCDebug(log_usb) << "Unexpected number of bytes received for brightness: " << result;
+        return -1;
+    }
+    
     return (data[0] << 8) | data[1];
+}
+
+void contrastTransferCallback(libusb_transfer *transfer) {
+     if (transfer->status != LIBUSB_TRANSFER_COMPLETED) {
+        qCDebug(log_usb) << "Transfer failed:" << libusb_error_name(transfer->status);
+        return;
+    }
+
+    uint8_t *data = transfer->buffer;
+    uint16_t contrast = (data[1] << 8) | data[0];
+    qCDebug(log_usb) << "Current Contrast: 0x" << QString::number(contrast, 16).rightJustified(4, '0');
+
+    libusb_free_transfer(transfer);
+}
+
+void USBControl::getContrastAsync() {
+    uint8_t bmRequestType = 0xA1;
+    uint8_t 	bRequest = GET_CUR;
+    uint16_t 	wValue = PU_BRIGHTNESS_CONTROL;
+    uint16_t 	wIndex = 0x0002;
+    uint16_t 	wLength = 2;
+    libusb_fill_control_setup(buffer, 
+                            bmRequestType,
+                            bRequest,
+                            wValue,
+                            wIndex,
+                            wLength
+    );
+    qCDebug(log_usb) << " After fill control setup! ";
+    libusb_fill_control_transfer(contrastTransfer, deviceHandle, buffer, contrastTransferCallback, NULL, 1000);
+    qCDebug(log_usb) << " After fill control tranfer! ";
+    
+}
+
+
+bool USBControl::initTransfer(){
+    contrastTransfer = libusb_alloc_transfer(0);
+    return true;
 }

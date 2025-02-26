506b6decbb06ae77df0ab3e1201eec5cc2de3d4a
Chow411 <2604590430@qq.com>
2025-02-26 11:16:33 +0800

    Add the click interval for keyboard click

--- scripts/KeyboardMouse.cpp
+++ scripts/KeyboardMouse.cpp
@@ -79,6 +79,7 @@ void KeyboardMouse::keyboardSend(){
     emit SerialPortManager::getInstance().sendCommandAsync(data, false);
     QThread::msleep(clickInterval);
     emit SerialPortManager::getInstance().sendCommandAsync(release, false);
+    QThread::msleep(clickInterval);
     // keyData.pop();
     // }
 }

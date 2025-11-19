2282561aa47110181123bfdf6a65582317882557
Chow411 <2604590430@qq.com>
2025-11-19 13:45:15 +0800

    Refactor CMake flags in build script for improved clarity and maintainability

--- build-script/build-static-qt-from-source.bat
+++ build-script/build-static-qt-from-source.bat
@@ -70,8 +70,8 @@ cmake -G "Ninja" ^
     -DCMAKE_C_FLAGS="-I%OPENSSL_INCLUDE_DIR%" ^
     -DCMAKE_CXX_FLAGS="-I%OPENSSL_INCLUDE_DIR%" ^
     -DCMAKE_EXE_LINKER_FLAGS="-mconsole -L%OPENSSL_LIB_DIR% -lws2_32 -lcrypt32 -ladvapi32" ^
-    -DCMAKE_TOOLCHAIN_FILE="%VCPKG_DIR%\scripts\buildsystems\vcpkg.cmake" ^
-    -DVCPKG_TARGET_TRIPLET=x64-mingw-static ^
+
+
     ..
 ninja
 ninja install
@@ -91,8 +91,8 @@ for %%m in (%MODULES%) do (
             -DOPENSSL_CRYPTO_LIBRARY="%OPENSSL_LIB_DIR%\libcrypto.a" ^
             -DOPENSSL_SSL_LIBRARY="%OPENSSL_LIB_DIR%\libssl.a" ^
             -DCMAKE_EXE_LINKER_FLAGS="-mconsole -L%OPENSSL_LIB_DIR%" ^
-            -DCMAKE_TOOLCHAIN_FILE="%VCPKG_DIR%\scripts\buildsystems\vcpkg.cmake" ^
-            -DVCPKG_TARGET_TRIPLET=x64-mingw-static ^
+
+
             ..
         ninja
         ninja install
